6e342a298b1e43f85279149870608678b2fa1cff
Andrew <65909640+Ecleziaste@users.noreply.github.com>
2020-12-06 23:14:05 +0600

    adaptive layout complete + some js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const leftWidget = document.querySelector('.image__widget_row');
+const rightWidget = document.querySelector('.footer__todo');
+
+const takeWidth = leftWidget.clientWidth;
+
+let changeWidth = () => { rightWidget.style.width = `${takeWidth}px`};
+// console.log(changeWidth);
+
+changeWidth();
