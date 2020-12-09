10d7000eeb53ae2df391ce91beeab79e1898eb67
Andrew <65909640+Ecleziaste@users.noreply.github.com>
2020-12-09 19:50:38 +0600

    added clock on the page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,54 @@
-'use strict';
+    'use strict';
 
-const leftWidget = document.querySelector('.image__widget_row');
-const rightWidget = document.querySelector('.footer__todo');
+    const leftWidget = document.querySelector('.image__widget_row');
+    const rightWidget = document.querySelector('.footer__todo');
 
-const takeWidth = leftWidget.clientWidth;
+//делаем ширину правого блока в футере такой же, как и у левого, чтоб Flex-box красиво отработал
+    const takeWidth = leftWidget.clientWidth;
+    let changeWidth = () => { rightWidget.style.width = `${takeWidth}px`};
+    changeWidth();
 
-let changeWidth = () => { rightWidget.style.width = `${takeWidth}px`};
-// console.log(changeWidth);
+//работает БЕЗ директивы use strict 
+    // window.onload = function(){
 
-changeWidth();
+    //     (function(){
+    //         let date = new Date();
+    //         let time = date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
+    //         document.querySelector('.time').innerHTML = time;
+    //         window.setTimeout(arguments.callee, 1000);
+    //     })();
+    
+    // };
+
+//работает С use strict   
+      /* функция добавления ведущих нулей */
+    /* (если число меньше десяти, перед числом добавляем ноль) */
+    //TODO: попробовать переписать на стрелочные, чтобы сократить код, убрать return
+    function zeroFirst(value) {
+        if (value < 10)
+        {
+            value='0'+value;
+        }
+        return value;
+    };
+
+    /* функция получения текущей даты и времени */
+    function getTime() {
+        let currentTime = new Date();
+        let hours = zeroFirst(currentTime.getHours());
+        let minutes = zeroFirst(currentTime.getMinutes());
+        // let seconds = zeroFirst(currentTime.getSeconds());
+        // return hours+":"+minutes+":"+seconds;
+        return hours+":"+minutes;
+    };
+
+    /* выводим текущую дату и время на сайт в нужный блок */
+    // document.querySelector('.time').innerHTML = getTime();
+
+    /* каждую секунду получаем текущую дату и время */
+    /* и вставляем значение в нужный блок  */
+    //TODO: сделать сетинтервал на минуту
+    setInterval(function () {
+        document.querySelector('.time').innerHTML = getTime();
+    }, 1000);
+        
